9540b2156970072835eec55096848e5bc803fc8c
jian-level-is <myunggeun22@gmail.com>
2022-05-16 20:29:33 +0900

    Spring boot Setting
    java 1.8
    Server start test (index.html)
    Add ignore ### Jian Add ###

---
+++ eroero/src/main/java/com/study/eroero/EroeroApplication.java
@@ -0,0 +1,13 @@
+package com.study.eroero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EroeroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EroeroApplication.class, args);
+	}
+
+}
