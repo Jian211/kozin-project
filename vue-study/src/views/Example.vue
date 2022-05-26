e8d3a625edb6058a2f16552ef6d272f410156794
jian-level-is <myunggeun22@gmail.com>
2022-05-26 17:22:56 +0900

    Rendering way
    v-for, v-if, v-show 사용법

--- vue-study/src/views/Example.vue
+++ vue-study/src/views/Example.vue
@@ -1,23 +1,13 @@
 <template>
-    <div>
-        <h1>속성에 데이터 바인딩</h1>
-        <!-- <img v-bind:src="url" style="height:200px" /> -->
-        <input type="text" v-model="textValue" />
-        <button type="button" v-bind:disabled="textValue==''" > Click </button>
-    </div>
+<div>
+    <h1 v-if="type=='A'">A</h1>
+    <h1 v-else-if="type=='B'">B</h1>
+    <h1 v-else>C</h1>
 
-    <div class="container" v-bind:class="{'active':isActive, 'text-red':isRed}">
-        <h1>클래스와 스타일에 대한 데이터 바인딩</h1>
-    </div>
-
-    <div class="container" v-bind:class="[activeClass, redClass]">
-        <h1>클래스와 스타일에 대한 데이터 바인딩 배열 사용</h1>
-    </div>
     <hr>
-    <div v-bind:style="[baseStyle, overridingStyle]">
-        인라인 스타일 Binding
-    </div>
 
+    <h1 v-show="bRender">T</h1>
+</div>
 </template>
 
 <script>
@@ -26,19 +16,8 @@ export default {
     components:{},
     data(){
         return {
-            url:"https://kr.vuejs.org/images/logo.png",
-            textValue :'',
-            isActive:true,
-            isRed: false,
-            activeClass:'active',
-            redClass:'text-red',
-            styleObject: {
-                backgroundColor: 'yellow',
-                color:'red',
-                fontWeigth:'bold'
-            },
-            baseStyle:'background-color:yellow;',
-            overridingStyle:'color:red; font-weigth:bold;'
+            type:'x',
+            bRender : true,
         };
     },
     setup(){},
@@ -50,17 +29,4 @@ export default {
 </script>
 
 <style scoped>
-.container {
-    width: 100%;
-    height: 200px;
-}
-
-.active{
-    background-color: yellow;
-    font-weight: bold;
-}
-
-.text-red{
-    color: red;
-}
 </style>
