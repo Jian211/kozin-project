3c98bebe29d7d1487ed8df2b2909765fe13cb326
jian-level-is <myunggeun22@gmail.com>
2022-05-29 19:22:08 +0900

    Computed와 Watch
    computed는 캐싱을 활용하여 데이터를 저장하는 형식,
    복잡한 작업은 추천하지 않으며, 간단한 작업 정도가 좋다.
    
    watch는 선언한 데이터를 관찰하고 있다가 변경이 감지되면 실행된다.  연산작업이나 복잡한 작업은 하지 않는 것이 좋다.

--- vue-study/src/views/Example.vue
+++ vue-study/src/views/Example.vue
@@ -1,13 +1,34 @@
 <template>
 <div>
-    <h1 v-if="type=='A'">A</h1>
-    <h1 v-else-if="type=='B'">B</h1>
-    <h1 v-else>C</h1>
+    <div>@click 이벤트를 통해 함수 연결 시켜보기</div>
+    <button type="button" @click="one(),two()">Add 1</button>
+    <p>The count is : {{counter}}</p>
+    <select v-model="city" @change="changeSelect">
+        <option value="서울">서울</option>
+        <option value="제주">제주</option>
+        <option value="부산">부산</option>
+    </select>
+    
+    <hr>
+    <div>
+        <input type="text" v-model="textValue" @keyup.enter="showValue">
+
+    </div>
+    <hr>
+    <h2>Computed란 </h2>
+    <input type="text" v-model="firstName" />
+    <input type="text" v-model="lastName" />
+    <h4>{{firstName + lastName}}</h4>
+    <h4>{{firstName}}{{lastName}}</h4>
+    <h3>{{fullName}}</h3>
 
     <hr>
-
-    <h1 v-show="bRender">T</h1>
+    <h2>Watch</h2> 
+    <input type="text" v-model.number="x1" @keyup="plusValue" />
+    <input type="text" v-model.number="x2" @keyup="plusValue" />
+    <input type="text" v-model.number="y"/>
 </div>
+
 </template>
 
 <script>
@@ -16,15 +37,62 @@ export default {
     components:{},
     data(){
         return {
-            type:'x',
-            bRender : true,
+            sampleData:'',
+            counter:0,
+            city:"",
+            textValue:'',
+            firstName:"지",
+            lastName:"명근",
+            x1:0,
+            x2:0,
+            y:0,
         };
+    },
+    watch:{
+        y(){
+            console.log(this.y+"로 값이 변경되었습니다.")
+            if(this.y > 50){
+                alert("50이 넘었습니다. 조심하십쇼. 0으로 초기화 됩니다.")
+                this.x1 = 0;
+                this.x2 = 0;
+                this.y = 0;
+            }
+        }
+    },
+    computed:{
+        fullName:{
+            get: function(){
+                return this.firstName + ' ' + this.lastName
+            },
+            set : function(newValue){
+                var names = newValue.split(' ');
+                console.log(names)
+                this.firstName = names[0]
+                this.lastName = names[names.length - 1]
+            }
+        }
     },
     setup(){},
     created(){},
-    mounted(){},
+    mounted(){    },
     unmounted(){},
-    methods:{}
+    methods:{
+        increaseCounter(){
+            this.counter += 1;
+        },
+        one(){
+
+        },
+        showValue(){
+            alert(this.textValue);
+        },
+        changeSelect(){
+            alert(this.city)
+        },
+        plusValue(){
+            return this.y = this.x1 + this.x2
+        }
+    }
 }
 </script>
 
