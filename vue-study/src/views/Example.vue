7b874d2f589057df2cb210c7a71c90d8734e2d48
jian-level-is <myunggeun22@gmail.com>
2022-05-29 23:00:12 +0900

    use sweetalert2 API

--- vue-study/src/views/Example.vue
+++ vue-study/src/views/Example.vue
@@ -1,34 +1,10 @@
 <template>
 <div>
-    <div>@click 이벤트를 통해 함수 연결 시켜보기</div>
-    <button type="button" @click="one(),two()">Add 1</button>
-    <p>The count is : {{counter}}</p>
-    <select v-model="city" @change="changeSelect">
-        <option value="서울">서울</option>
-        <option value="제주">제주</option>
-        <option value="부산">부산</option>
-    </select>
-    
-    <hr>
-    <div>
-        <input type="text" v-model="textValue" @keyup.enter="showValue">
-
-    </div>
-    <hr>
-    <h2>Computed란 </h2>
-    <input type="text" v-model="firstName" />
-    <input type="text" v-model="lastName" />
-    <h4>{{firstName + lastName}}</h4>
-    <h4>{{firstName}}{{lastName}}</h4>
-    <h3>{{fullName}}</h3>
-
-    <hr>
-    <h2>Watch</h2> 
-    <input type="text" v-model.number="x1" @keyup="plusValue" />
-    <input type="text" v-model.number="x2" @keyup="plusValue" />
-    <input type="text" v-model.number="y"/>
+    <input type="text" v-model="userInfo.name">
+    <input type="text" v-model.number="userInfo.age">
+    <input type="text" v-model="userInfo.job">
+    <button @click="saveUserInfo">Save</button>
 </div>
-
 </template>
 
 <script>
@@ -37,62 +13,42 @@ export default {
     components:{},
     data(){
         return {
-            sampleData:'',
-            counter:0,
-            city:"",
-            textValue:'',
-            firstName:"지",
-            lastName:"명근",
-            x1:0,
-            x2:0,
-            y:0,
+            userInfo:{
+                name:'',
+                age:'',
+                job:'',
+            }
         };
-    },
-    watch:{
-        y(){
-            console.log(this.y+"로 값이 변경되었습니다.")
-            if(this.y > 50){
-                alert("50이 넘었습니다. 조심하십쇼. 0으로 초기화 됩니다.")
-                this.x1 = 0;
-                this.x2 = 0;
-                this.y = 0;
-            }
-        }
-    },
-    computed:{
-        fullName:{
-            get: function(){
-                return this.firstName + ' ' + this.lastName
-            },
-            set : function(newValue){
-                var names = newValue.split(' ');
-                console.log(names)
-                this.firstName = names[0]
-                this.lastName = names[names.length - 1]
-            }
-        }
     },
     setup(){},
     created(){},
-    mounted(){    },
+    mounted(){},
     unmounted(){},
     methods:{
-        increaseCounter(){
-            this.counter += 1;
+        saveUserInfo: function(){
+            if(this.userInfo.name == ''){
+                // return alert('사용자 이름을 입력하세요.');
+                return this.$swal('사용자 이름을 입력하세요.')
+            }
+            if(this.userInfo.age == 0){
+                return alert('사용자 나이를 입력하세요.');
+            }
+            if(this.userInfo.job == ''){
+                return alert('사용자 직업을 입력하세요.');
+            }
+            const r = this.saveData(this.userInfo);
+            if(r == 'S'){
+                alert('사용자 정보가 생성되었습니다.');
+            }
         },
-        one(){
+        saveData: function(params){
+            console.log(params);
+            const r = 'S';
+            return r;
+        },
 
-        },
-        showValue(){
-            alert(this.textValue);
-        },
-        changeSelect(){
-            alert(this.city)
-        },
-        plusValue(){
-            return this.y = this.x1 + this.x2
-        }
     }
+
 }
 </script>
 
---
+++ vue-study/src/views/search.vue
@@ -0,0 +1,79 @@
+<template>
+<div>
+    <div>
+        <select v-model="selectedCity">
+            <option :key="i" :value="city.code" v-for="(city,i) in cityList">{{city.name}}</option>
+        </select>
+        <input type="text" v-model="userName" @keyup.enter="searchUserList"/>
+        <button type="button" @click="searchUserList">조회</button>
+    </div>
+    <div>
+        <table>
+            <thead>
+                <tr>
+                    <th>이름</th>
+                    <th>나이</th>
+                    <th>직업</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr :key="i" v-for="(person,i) in userList" >
+                    <td>{{person.name}}</td>
+                    <td>{{person.age}}</td>
+                    <td>{{person.job}}</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+
+</div>
+</template>
+
+<script>
+export default {
+    name:'sample-name',
+    components:{},
+    data(){
+        return {
+            cityList:[
+                {name:'서울', code: '02'},
+                {name:'부산', code: '21'},
+                {name:'제주', code: '064'},
+            ],
+            selectedCity:'',
+            userList:[],
+        };
+    },
+    setup(){},
+    created(){},
+    mounted(){},
+    unmounted(){},
+    methods:{
+        searchUserList(){
+            // const selectedCity = this.selectedCity;
+            // const userName = this.userName;
+            const params = {
+                selectedCity : this.selectedCity,
+                userName : this.userName
+            }
+
+            this.userList = this.getUserList(params);
+        },
+        getUserList(params){
+            console.log(params)
+
+            let list = [
+                {name : '유재석', age:'49', job:'연예인'},
+                {name : '하하', age:'23', job:'연예인'},
+                {name : '정준하', age:'48', job:'연예인'},
+                {name : '양세찬', age:'25', job:'연예인'},
+            ];
+
+            return list;
+        }
+    }
+}
+</script>
+
+<style scoped>
+</style>
